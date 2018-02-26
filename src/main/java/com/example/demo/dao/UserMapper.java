e97722a07c290970c01e153a17c787d656a2f417
zhuxin5 <zhuxin5@jd.com>
2018-02-26 17:51:12 +0800

    更新1

--- src/main/java/com/example/demo/dao/UserMapper.java
+++ src/main/java/com/example/demo/dao/UserMapper.java
@@ -1,4 +1,4 @@
-package com.example.demo.mapper;
+package com.example.demo.dao;
 
 import com.example.demo.domain.User;
 import org.apache.ibatis.annotations.*;
--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -1,7 +1,7 @@
 package com.example.demo.service.impl;
 
 import com.example.demo.domain.User;
-import com.example.demo.mapper.UserMapper;
+import com.example.demo.dao.UserMapper;
 import com.example.demo.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
